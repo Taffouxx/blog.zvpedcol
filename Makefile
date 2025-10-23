157853db9c898cd3112bb681988527a1ab0f61ee
Izuk jl <cergeyyarovikov82@gmail.com>
2025-10-23 19:02:55 +0300

    deploy

--- Makefile
+++ Makefile
@@ -2,13 +2,13 @@
 
 NOTION_PAGE_ID= :=
 setup:
-	docker build . -t morethan-log ; \
-	docker run -it --rm -v $(PWD):/app morethan-log /bin/bash -c "yarn install" ; \
+	docker build . -t zvpedcol-blog ; \
+	docker run -it --rm -v $(PWD):/app zvpedcol-blog /bin/bash -c "yarn install" ; \
 	echo NOTION_PAGE_ID=$(NOTION_PAGE_ID) > .env.local
 
 dev:
-	docker run -it --rm -v $(PWD):/app -p 8001:3000 morethan-log /bin/bash -c "yarn run dev"
+	docker run -it --rm -v $(PWD):/app -p 8001:3000 zvpedcol-blog /bin/bash -c "yarn run dev"
 
 run:
-	docker run -it --rm -v $(PWD):/app morethan-log /bin/bash
+	docker run -it --rm -v $(PWD):/app zvpedcol-blog /bin/bash
 
--- site.config.js
+++ site.config.js
@@ -1,32 +1,29 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Жовтоводьский Фаховий Педагогічний Коледж ДОР",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "офіційний сайт",
+    bio: "Офіційний блог Жовтоводьский Фаховий Педагогічний Коледж ДОР.",
+    email: "",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [
-    {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
-    },
+    // Empty or add relevant college projects/links here
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Жовтоводьский Фаховий Педагогічний Коледж ДОР",
+    description: "Офіційний блог Жовтоводьский Фаховий Педагогічний Коледж ДОР",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "uk-UA", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
