8e804ddfc9917d9786bd73b5145c4440bb1a75be
Izuk jl <cergeyyarovikov82@gmail.com>
2025-10-23 21:30:43 +0300

    Improve: Дал

--- src/routes/Detail/components/NotionRenderer/index.tsx
+++ src/routes/Detail/components/NotionRenderer/index.tsx
@@ -1,82 +1,22 @@
-import dynamic from "next/dynamic"
-import Image from "next/image"
-import Link from "next/link"
-import { ExtendedRecordMap } from "notion-types"
-import useScheme from "src/hooks/useScheme"
-import { FC } from "react"
-import styled from "@emotion/styled"
+import "react-notion-x/src/styles.css";
+import "katex/dist/katex.min.css";
+import "prismjs/themes/prism-tomorrow.css";
 
-const _NotionRenderer = dynamic(
-  () => import("react-notion-x").then((m) => m.NotionRenderer),
-  { ssr: false }
-)
+import { AppPropsWithLayout } from "src/types";
+import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
+import { RootLayout } from "src/layouts";
+import { queryClient } from "src/libs/react-query";
 
-const Code = dynamic(() =>
-  import("react-notion-x/build/third-party/code").then(async (m) => m.Code)
-)
+function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page: JSX.Element) => page);
 
-const Collection = dynamic(() =>
-  import("react-notion-x/build/third-party/collection").then(
-    (m) => m.Collection
-  )
-)
-const Equation = dynamic(() =>
-  import("react-notion-x/build/third-party/equation").then((m) => m.Equation)
-)
-const Pdf = dynamic(
-  () => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf),
-  {
-    ssr: false,
-  }
-)
-const Modal = dynamic(
-  () => import("react-notion-x/build/third-party/modal").then((m) => m.Modal),
-  {
-    ssr: false,
-  }
-)
-
-const mapPageUrl = (id: string) => {
-  return "https://www.notion.so/" + id.replace(/-/g, "")
-}
-
-type Props = {
-  recordMap: ExtendedRecordMap
-}
-
-const NotionRenderer: FC<Props> = ({ recordMap }) => {
-  const [scheme] = useScheme()
   return (
-    <StyledWrapper>
-      <_NotionRenderer
-        darkMode={scheme === "dark"}
-        recordMap={recordMap}
-        components={{
-          Code,
-          Collection,
-          Equation,
-          Modal,
-          Pdf,
-          nextImage: Image,
-          nextLink: Link,
-        }}
-        mapPageUrl={mapPageUrl}
-      />
-    </StyledWrapper>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
-export default NotionRenderer
-
-const StyledWrapper = styled.div`
-  /* // TODO: why render? */
-  .notion-collection-page-properties {
-    display: none !important;
-  }
-  .notion-page {
-    padding: 0;
-  }
-  .notion-list {
-    width: 100%;
-  }
-`
+export default App;
